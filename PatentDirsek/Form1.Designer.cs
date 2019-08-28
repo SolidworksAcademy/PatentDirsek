cd4dc8d319d25725049c5d919903f5ac271f5afd
mozguc <hmuratozguc@gmail.com>
2019-08-28 17:50:14 +0300

    Form Edit

--- PatentDirsek/Form1.Designer.cs
+++ PatentDirsek/Form1.Designer.cs
@@ -35,15 +35,13 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_CreateModel
             // 
             this.btn_CreateModel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.btn_CreateModel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn_CreateModel.Location = new System.Drawing.Point(83, 183);
+            this.btn_CreateModel.Location = new System.Drawing.Point(83, 196);
             this.btn_CreateModel.Margin = new System.Windows.Forms.Padding(2);
             this.btn_CreateModel.Name = "btn_CreateModel";
             this.btn_CreateModel.Size = new System.Drawing.Size(171, 43);
@@ -54,83 +52,62 @@
             // 
             // txt_cap
             // 
-            this.txt_cap.Location = new System.Drawing.Point(109, 37);
+            this.txt_cap.Location = new System.Drawing.Point(124, 37);
             this.txt_cap.Margin = new System.Windows.Forms.Padding(2);
             this.txt_cap.Name = "txt_cap";
-            this.txt_cap.Size = new System.Drawing.Size(145, 33);
+            this.txt_cap.Size = new System.Drawing.Size(130, 33);
             this.txt_cap.TabIndex = 1;
             // 
             // txt_kalinlik
             // 
-            this.txt_kalinlik.Location = new System.Drawing.Point(109, 75);
+            this.txt_kalinlik.Location = new System.Drawing.Point(124, 75);
             this.txt_kalinlik.Margin = new System.Windows.Forms.Padding(2);
             this.txt_kalinlik.Name = "txt_kalinlik";
-            this.txt_kalinlik.Size = new System.Drawing.Size(145, 33);
+            this.txt_kalinlik.Size = new System.Drawing.Size(130, 33);
             this.txt_kalinlik.TabIndex = 2;
             // 
             // txt_Radius
             // 
-            this.txt_Radius.Location = new System.Drawing.Point(109, 113);
+            this.txt_Radius.Location = new System.Drawing.Point(124, 113);
             this.txt_Radius.Margin = new System.Windows.Forms.Padding(2);
             this.txt_Radius.Name = "txt_Radius";
-            this.txt_Radius.Size = new System.Drawing.Size(145, 33);
+            this.txt_Radius.Size = new System.Drawing.Size(130, 33);
             this.txt_Radius.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 39);
+            this.label1.Location = new System.Drawing.Point(21, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.Size = new System.Drawing.Size(103, 25);
             this.label1.TabIndex = 4;
-            this.label1.Text = "Çap : ";
+            this.label1.Text = "Diameter : ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(17, 76);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.Size = new System.Drawing.Size(107, 25);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Kalınlık : ";
+            this.label2.Text = "Thickness : ";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 114);
+            this.label3.Location = new System.Drawing.Point(42, 113);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "Radius :";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(626, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 42);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Skirt";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(626, 83);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 45);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(754, 272);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(288, 272);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -156,8 +133,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
